b296efad93f5971b950754e20373ea03a3dd358e
bobadillaFacundo <57062710+bobadillaFacundo@users.noreply.github.com>
2025-07-01 18:15:46 -0300

    .

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,7 +5,6 @@ import { verification } from "../models/user.model.js";
 export async function login(req, res) {
   const user = req.body;
   const authenticatedUser = await verification(user.email, user.password);
-  console.log("Authenticated User:", authenticatedUser);
   
   if (authenticatedUser) {
     const token = generateToken(authenticatedUser);
